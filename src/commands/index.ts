d6f225651718bed5a5ffd4a920cba8cfb70d4e56
Nick Seman <me@vphoebe.dev>
2023-08-20 15:22:51 -0700

    feat: add rules command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,5 +2,6 @@ import { CommandDefinition } from "../types.js";
 import move from "./move.js";
 import pass from "./pass.js";
 import start from "./start.js";
+import rules from "./rules.js";
 
-export const enabledCommands: CommandDefinition[] = [start, move, pass];
+export const enabledCommands: CommandDefinition[] = [start, move, pass, rules];
